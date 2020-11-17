70e66961eb8244d09f3fbf1aa6f9d463f46870ba
zzamai <vinicius.zamai@outlook.com>
2020-11-17 13:59:23 -0300

    eslint configs

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # React_Native_Clone_Instagram
-Clone do Instagram usando React Native
+### Clone do Instagram usando React Native
+##### Projeto que visa o aprendizado em React Native feito pela plataforma da DIO - Digital Innovation One
---
+++ cloneInstagram/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      
+    </>
+  );
+};
+
+
+export default App;

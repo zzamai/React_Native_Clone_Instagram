ef63af0ad58667a173019822477f1eff408865c5
zzamai <vinicius.zamai@outlook.com>
2020-11-22 23:30:20 -0300

    get da api funcionando

---
+++ cloneInstagram/src/Pages/Feed/styles.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View``;
+
+export const Post = styled.View``;
+
+export const Header = styled.View``;
+
+export const Avatar = styled.Image``;
+
+export const Name = styled.Text``;
+
+export const Description = styled.Text``;
+
+export const Loading = styled.ActivityIndicator.attrs({
+    size: 'small',
+    color: '#999',
+})`
+    margin: 30px 0;
+`;
---
+++ cloneInstagram/src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Image } from 'react-native';
+import Logo from '../../assets/logo.png';
+import { Container } from './styles';
+
+export default function Header() {
+    return (
+        <Container>
+            <Image source={Logo} />
+        </Container>
+    );
+}
